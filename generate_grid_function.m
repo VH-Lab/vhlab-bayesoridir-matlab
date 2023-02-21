83e7684c37eac0841621fc99f8d2c51bd706e345
ztwuu <ztwu@brandeis.edu>
2023-02-20 19:37:31 -0500

    generate bayes grid function

---
+++ generate_grid_function.m
@@ -0,0 +1,6 @@
+function out = generate_grid_function(~)
+
+out.RP = 85:155;
+out.OP = 1:360;
+out.RN = 25:95;
+out.SIG = 40:100;
--- response_value_data.m
+++ response_value_data.m
@@ -8,9 +8,8 @@ SIG = 70;
 offset = RP/5;
 SIG_offset = 5;
 
-if (theta >=0 && theta <360 && rem(theta,1)==0)
-    input(theta).RSP = RP*exp(-1/2.*(theta-OP).^2/SIG.^2)+RN*exp(-1/2.*(theta-ON).^2/SIG.^2)+offset+SIG_offset*randn(1);
-
+if (theta >0 && theta <=360 && rem(theta,1)==0)
+    input = RP*exp(-1/2.*(theta-OP).^2/SIG.^2)+RN*exp(-1/2.*(theta-ON).^2/SIG.^2)+offset+SIG_offset*randn(1);
 else
-    error = 'input error. This is not a valid value of theta'
+    error = 'input error. This is not a valid value of theta.'
 end
