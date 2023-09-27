9ec1a89f970e350bd1827872f71f39bb3f5d0717
ztwuu <ztwu@brandeis.edu>
2023-09-27 19:03:44 -0400

    simulate noisy tuning curve test

--- thesis/test1.m
+++ thesis/test1.m
@@ -3,16 +3,12 @@ parameters = data.generate_fixed_parameters(7,6,270,40,3);
 ang = 0:45:359;
 data1 = data.generate_simulate_data(ang,parameters);
 [data2,noise] = data.generate_noise(data1,5);
+
+%plotting
 figure(1),hold on
-plot(ang,data2(:,1),'r')
-plot(ang,data2(:,2),'g')
-plot(ang,data2(:,3),'b')
-plot(ang,data2(:,4),'y')
-plot(ang,data2(:,5),'c')
-parameters = simulation.generate_fixed_parameters(7,6,270,40,3);
-ang = 0:45:359;
-data1 = simulation.generate_simulate_data(ang,parameters);
-[data2,noise] = simulation.generate_noise(data1,1);
-figure(1),hold on
-plot(ang,data2,'r')
-plot(ang,data1,'k')
+plot(ang,data2,'r*') 
+plot(ang,data1,'k.')
+ylim([0,15])
+xlabel('theta')
+ylabel('response magnitude')
+title('simulate tuning curve (+50% noise)')
--- thesis/tools/+data/generate_noise.m
+++ thesis/tools/+data/generate_noise.m
@@ -1,16 +1,17 @@
-function [noisy_curves,noise] = generate_noise(raw_curve,curve_num)
+function [noisy_curve,noise] = generate_noise(raw_curve,curve_num)
 % generate <curve_num> tunning curves with random gaussian noise
 
 %   Inputs: RAW_CURVE - 
 %           CURVE_NUM - 
 %
-%   Output: NOISY_CURVES -
+%   Output: NOISY_CURVE - 
 %           NOISE - N(raw_datapoint,1)
 
 if size(raw_curve,1) < size(raw_curve,2)
     raw_curve = raw_curve';
 end
-noisy_curves = zeros(length(raw_curve),curve_num);
+noisy_curve = zeros(length(raw_curve),curve_num);
 
-noise = 0.5 .* raw_curve .* randn(size(noisy_curves));
-noisy_curves = raw_curve + noise;
+noise = 0.5 .* raw_curve .* randn(size(noisy_curve));
+noisy_curve = mean(raw_curve + noise,2);
+end
