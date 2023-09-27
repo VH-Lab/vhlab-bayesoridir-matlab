a3db8cc9f98b5cbf2f102f006a808da4fa906b2f
ztwuu <ztwu@brandeis.edu>
2023-09-27 14:47:47 -0400

    small update

--- thesis/test1.m
+++ thesis/test1.m
@@ -9,4 +9,10 @@ plot(ang,data2(:,2),'g')
 plot(ang,data2(:,3),'b')
 plot(ang,data2(:,4),'y')
 plot(ang,data2(:,5),'c')
+parameters = simulation.generate_fixed_parameters(7,6,270,40,3);
+ang = 0:45:359;
+data1 = simulation.generate_simulate_data(ang,parameters);
+[data2,noise] = simulation.generate_noise(data1,1);
+figure(1),hold on
+plot(ang,data2,'r')
 plot(ang,data1,'k')
