c8a89bdbb3a67890333e81cfe7e7a16c93bfcd17
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2023-11-17 15:39:10 -0500

    updated fit_proportional_noise

--- +vis/+bayes/+noise/fit_proportional_noise.m
+++ +vis/+bayes/+noise/fit_proportional_noise.m
@@ -19,10 +19,14 @@ v2 = v(index);
 
 md1 = fitlm(log10(m2),log10(v2));
 
-proportional_noise = ml.Coefficients{:,1};
+proportional_noise = md1.Coefficients{:,1};
 
 if plotit,
   % plot m2, v2, and the fit
-
-
+    offset = proportional_noise(1);
+    slope = proportional_noise(2);
+    x = logspace(-3,4,100);
+    y = 10.^offset*x.^slope;
+    plot(x,y,'o')
+    set(gca,'xscale','log','yscale','log')
 end;
